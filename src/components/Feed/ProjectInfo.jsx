54e5b7cfb7891b28fc0c788af19b1a519e89991f
Vipul Sharma <Vipulsnp@gmail.com>
2020-08-20 14:39:24 +0530

    Minor change in prop validation (#197)

--- src/components/Feed/ProjectInfo.jsx
+++ src/components/Feed/ProjectInfo.jsx
@@ -164,7 +164,7 @@ ProjectInfo.defaultProps = {
 ProjectInfo.propTypes = {
   page: PropTypes.string.isRequired,
   url: PropTypes.string,
-  data: PropTypes.shapeOf({
+  data: PropTypes.exact({
     issues: PropTypes.array,
     pulls: PropTypes.array
   })

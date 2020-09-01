298f8330297456ac3aebff82f7c3c16cc6d05a14
Vipul Sharma <Vipulsnp@gmail.com>
2020-09-01 15:41:50 +0530

    Minor changes (#210)

--- pages/login.jsx
+++ pages/login.jsx
@@ -4,60 +4,52 @@
   If auth is failed then it will redirect to homepage.
 */
 import Axios from 'axios';
-import Router from 'next/router';
-import React, { useEffect , useContext } from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useContext } from 'react';
 
-import * as authFunctions from '../src/api/authFunctions'; 
+import * as authFunctions from '../src/api/authFunctions';
 import Spinner from '../src/components/Spinner';
 import UserContext from '../src/components/UserContext';
- 
-
 
 export default function loginMiddleWare() {
-
   const { setUser } = useContext(UserContext);
-
+  const router = useRouter();
   useEffect(() => {
-    if (Router.query.status === "200") {
-        // Set logged In here
-        // Redirect to feed page
-      Axios.get(
-        `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/v1/user/profile`,
-        {
-          headers: {
-            Authorization: `Bearer ${  Router.query.token}`
-          }
+    if (router.query.status === '200') {
+      // Set logged In here
+      // Redirect to feed page
+      console.log(router.query.token);
+      localStorage.setItem('token', router.query.token);
+      Axios.get(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/v1/user/profile`, {
+        headers: {
+          Authorization: `Bearer ${router.query.token}`
         }
-      ).then((res) => {
+      })
+        .then((res) => {
+          // store the data in the local Storage
+          const user = {
+            // eslint disable-next-line
+            uid: res.data.data._id,
+            name: res.data.data.name,
+            profileImageUrl: res.data.data.profileImage
+          };
 
-                      // store the data in the local Storage
-        const user = {
+          localStorage.setItem('user', authFunctions.secureToken(user));
+          // Set the USer Context for the New User here
           // eslint disable-next-line
-          uid: res.data.data._id,
-          name: res.data.data.name,
-          profileImageUrl: res.data.data.profileImage,
-        };
-        console.log(Router.query.token);
-        localStorage.setItem('token', Router.query.token);
-
-        localStorage.setItem('user', authFunctions.secureToken(user));
-                      // Set the USer Context for the New User here
-                      // eslint disable-next-line
-        setUser({
-          uid: res.data.data._id,
-          name: res.data.data.name,
-          profileImageUrl: res.data.data.profileImage
+          setUser({
+            uid: res.data.data._id,
+            name: res.data.data.name,
+            profileImageUrl: res.data.data.profileImage
+          });
+        })
+        .catch(() => {
+          authFunctions.logout();
         });
-      }).catch( ()=> {
-        authFunctions.logout();;
-      });
-      Router.replace('/feed');
-    }
-    else
-    {
+      router.replace('/feed');
+    } else {
       authFunctions.logout();
     }
   }, []);
   return <Spinner />;
 }
-      

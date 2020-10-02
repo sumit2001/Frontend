fbf403e2fe8ebe6c17e52b8f6ae16c2f02e2a3e3
Vipul Sharma <Vipulsnp@gmail.com>
2020-10-02 20:09:08 +0530

    Fixed Bugs on Homepage (#235)

--- src/components/subscribe.jsx
+++ src/components/subscribe.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import styles from '../scss/subscribe.module.scss';
 
 export default function Subscribe() {
-
   return (
     <div>
       {/* ===========================================================================================
@@ -11,12 +10,12 @@ export default function Subscribe() {
           =========================================================================================== */}
 
       <div className={styles['subscribe-container']}>
-        <img src="SVG/subscribe-top.svg" alt=""/>
+        <img src="SVG/subscribe-top.svg" alt="" />
         <div className={styles['subscribe-inner']}>
-          <img src='SVG/subscribe-top-girl.svg' alt=''/>
+          <img src="SVG/subscribe-top-girl.svg" alt="" />
           <h2> Stay updated to our Newsletter</h2>
           <div className={styles['subscribe-input']}>
-            <input type="email" placeholder="Enter your email"/>
+            <input type="email" placeholder="Enter your email" />
             <button type="button" className={styles['subscribe-button']}>
               SUBSCRIBE
             </button>

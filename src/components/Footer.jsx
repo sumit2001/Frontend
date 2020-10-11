2f6b6ba65a76ddfbd3788168e6702e0686f254d4
Thomas Vaeth <thomas.vaeth@gmail.com>
2020-10-11 01:27:53 -0700

    Mobile First Footer Styling (#243)
    * Made footer styling mobile first.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,49 +15,63 @@ export default function Footer() {
           Contact Us
           <hr/>
         </h1>
-        <div className={styles['footer-content']}>
-          <div className={styles.badges}>
-            {/* Dev.to */}
+        <ul className={styles.badges}>
+          {/* Dev.to */}
+          <li>
             <a href="https://dev.to/_opensourcecode">
               <img src="/SVG/dev_to.svg" alt="Dev.to"/>
               Dev.to
             </a>
-            {/* Discord */}
+          </li>
+          {/* Discord */}
+          <li>
             <a href="https://discord.gg/HYPt5RX">
               <img src="/SVG/footer-discord.svg" alt="Discord"/>
               Discord
             </a>
-            {/* Twitter */}
+          </li>
+          {/* Twitter */}
+          <li>
             <a href="https://twitter.com/_opensourcecode">
               <img src="/SVG/footer-twitter.svg" alt="Twitter"/>
               Twitter
             </a>
-            {/* Github */}
+          </li>
+          {/* Github */}
+          <li>
             <a href="https://github.com/OpenSouceCode">
               <img src="/SVG/footer-github.svg" alt="Github"/>
               Github
             </a>
-            {/* LinkedIn */}
+          </li>
+          {/* LinkedIn */}
+          <li>
             <a href="https://www.linkedin.com/company/opensourcecode">
               <img src="/SVG/footer-linkedin.svg" alt="LinkedIn"/>
               LinkedIn
             </a>
-            {/* Slack */}
+          </li>
+          {/* Slack */}
+          <li>
             <a href="https://join.slack.com/t/opensourcecode/shared_invite/zt-f0frpe9g-8fGLNGPA6HUQR~XuCoAJmg">
               <img src="/SVG/footer-slack.svg" alt="Slack"/>
               Slack
             </a>
+          </li>
 
+          <li>
             <a href="https://github.com/OpenSouceCode/Frontend/tree/master/src/components">
               <img src="/SVG/pencil.svg" alt="pencil"/>
               Edit this page
             </a>
+          </li>
+          <li>
             <a href="https://github.com/OpenSouceCode/Frontend">
               <img src="/SVG/code.svg" alt="</>"/>
               View Source Code
             </a>
-          </div>
-        </div>
+          </li>
+        </ul>
         <div className={styles['partner-logo']}>
           <img src="SVG/footer-partner-logo.svg" alt="Powered by InCampus"/>
         </div>

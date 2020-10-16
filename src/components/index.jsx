090853c0944a438f0417c5e9a7a71e5a8dcbbbcd
Shreya Gupta <shreyagupta72484@gmail.com>
2020-10-16 21:27:33 +0530

    Create index.jsx (#250)
    * Create index.jsx
    
    for reducing multiple export statements

---
+++ src/components/index.jsx
@@ -0,0 +1,11 @@
+import Carousel from './Carousel';
+import Footer from './Footer';
+import Header from './Header';
+import AboutUs from './Home/AboutUs';
+import HelpUs from './Home/HelpUs';
+import HowContainer from './Home/HowContainer';
+import WelcomeComponent from './Home/WelcomeComponent';
+import Spinner from './Spinner';
+import Subscribe from './subscribe';
+
+export {Carousel, Footer,Header, AboutUs, HelpUs, HowContainer, WelcomeComponent, Spinner, Subscribe};

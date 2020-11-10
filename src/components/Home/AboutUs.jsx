9d989441cbe397f2ac7acc3c119fb9667b29fce0
Arya Soni <aryasoni98@gmail.com>
2020-11-10 20:56:27 +0530

    Update Landing Page

--- src/components/Home/AboutUs.jsx
+++ src/components/Home/AboutUs.jsx
@@ -22,13 +22,12 @@ export default function AboutUs() {
           </h1>
 
           <p>
-            The idea of CodeTrophs excites you but not sure where to begin or
-            which project to choose? Often feel confused and uncomfortable while
-            using GitHub, and can’t think of any available alternative? Well,
-            you have arrived at the correct place! &#34; CodeTrophs &#34;
-            is an open source platform targeted solely for beginners to help
-            them contribute to real life projects, from developing new ideas and
-            approaches, to exploring the world of open sourcing.
+          Does the idea of open source excite you but not sure where to begin or which project to choose? 
+          Often feel confused and uncomfortable while using GitHub, and can’t think of any available alternative? 
+          Well, you have arrived at the correct place &#34; CodeTrophs &#34; is an open source platform targeted solely 
+          for beginners to help them contribute to real life projects, from developing new ideas and approaches, 
+          to exploring the world of open sourcing.
+            &#34;
           </p>
         </div>
       </div>
